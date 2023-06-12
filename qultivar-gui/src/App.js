9323ed6a78ec960577bb9fe917a4504066afa36b
Allan Heekes <allan.heekes@csgi.com>
2023-06-12 21:56:58 +0200

    added the explorer and the grid panel code

--- qultivar-gui/src/App.js
+++ qultivar-gui/src/App.js
@@ -6,73 +6,74 @@ import UserGrid from './UserGrid';
 import GrowStageGrid from './GrowStageGrid';
 import GrowGrid from './GrowGrid';
 import GrowEditPage from './GrowEditPage';
+import './App.css';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [token, setToken] = useState('');
-  const [email, setEmail] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [token, setToken] = useState('');
+    const [email, setEmail] = useState('');
 
-  useEffect(() => {
-    const storedEmail = localStorage.getItem('email');
-    const storedToken = localStorage.getItem('token');
-    if (storedToken && storedEmail) {
-      setIsLoggedIn(true);
-      setEmail(storedEmail);
-      setToken(storedToken);
-    }
-  }, []);
+    useEffect(() => {
+        const storedEmail = localStorage.getItem('email');
+        const storedToken = localStorage.getItem('token');
+        if (storedToken && storedEmail) {
+            setIsLoggedIn(true);
+            setEmail(storedEmail);
+            setToken(storedToken);
+        }
+    }, []);
 
-  const handleLogin = (userEmail, userToken) => {
-    setIsLoggedIn(true);
-    setEmail(userEmail);
-    setToken(userToken);
-    localStorage.setItem('email', userEmail);
-    localStorage.setItem('token', userToken);
-  };
+    const handleLogin = (userEmail, userToken) => {
+        setIsLoggedIn(true);
+        setEmail(userEmail);
+        setToken(userToken);
+        localStorage.setItem('email', userEmail);
+        localStorage.setItem('token', userToken);
+    };
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setToken('');
-    setEmail('');
-    localStorage.removeItem('token');
-    localStorage.removeItem('email');
-  };
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+        setToken('');
+        setEmail('');
+        localStorage.removeItem('token');
+        localStorage.removeItem('email');
+    };
 
-  return (
-    <Router>
-      <div>
-        {isLoggedIn ? (
-          <div>
-            <h1>Navigation Menu</h1>
-            <div>
-              <ul>
-                <li>
-                  <Link to="/users">Users</Link>
-                </li>
-                <li>
-                  <Link to="/growStages">Grow Stages</Link>
-                </li>
-                <li>
-                  <Link to="/grows">Grows</Link>
-                </li>
-              </ul>
+    return (
+        <Router>
+            <div className="app-container">
+                {isLoggedIn ? (
+                    <div className="panel-container">
+                        <div className="navigation-panel">
+                            <h1>Navigation Menu</h1>
+                            <ul>
+                                <li>
+                                    <Link to="/users">Users</Link>
+                                </li>
+                                <li>
+                                    <Link to="/growStages">Grow Stages</Link>
+                                </li>
+                                <li>
+                                    <Link to="/grows">Grows</Link>
+                                </li>
+                            </ul>
+                            <button onClick={handleLogout}>Logout</button>
+                        </div>
+                        <div className="grid-panel">
+                            <Routes>
+                                <Route path="/users" element={<UserGrid token={token} />} />
+                                <Route path="/growStages" element={<GrowStageGrid token={token} />} />
+                                <Route path="/grows" element={<GrowGrid email={email} token={token} />} />
+                                <Route path="/grow/edit/:growId" element={<GrowEditPage token={token} />} />
+                            </Routes>
+                        </div>
+                    </div>
+                ) : (
+                    <LoginPage handleLogin={handleLogin} />
+                )}
             </div>
-
-            <Routes>
-              <Route path="/users" element={<UserGrid token={token} />} />
-              <Route path="/growStages" element={<GrowStageGrid token={token} />} />
-              <Route path="/grows" element={<GrowGrid email={email} token={token} />} />
-              <Route path="/grow/edit/:growId" element={<GrowEditPage token={token} />} />
-            </Routes>
-
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-        ) : (
-          <LoginPage handleLogin={handleLogin} />
-        )}
-      </div>
-    </Router>
-  );
+        </Router>
+    );
 };
 
 export default App;
